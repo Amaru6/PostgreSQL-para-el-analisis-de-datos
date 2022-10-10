0f75165699f720eca2225e29dd724e7fc7dcaa1f
Amarullazo <julio.collazos@unmsm.edu.pe>
2022-10-10 16:36:11 -0500

    additional window understanding!

--- projecto/capitulos/07_window-functions-and-subqueries_10-10-2022/lecture_07.sql
+++ projecto/capitulos/07_window-functions-and-subqueries_10-10-2022/lecture_07.sql
@@ -166,3 +166,57 @@ FROM
 	customer_purchases
 GROUP BY
 	market_date;
+	
+-- More about WINDOW Functions
+
+CREATE TABLE widget_companies(
+	id bigserial,
+	company varchar(30) NOT NULL,
+	widget_output integer NOT NULL
+);
+
+INSERT INTO widget_companies (company, widget_output)
+VALUES
+	('Morse Widgets', 125000),
+	('Springfield Widget Masters', 143000),
+	('Best Widgets', 196000),
+	('Acme Inc.', 133000),
+	('District Widget Inc.', 201000),
+	('Clarke Amalgamted', 244000),
+	('Stavesacre Industries', 244000),
+	('Bowers Widget Emporium', 201000);
+	
+SELECT
+	wc.*,
+	RANK() OVER (ORDER BY widget_output) AS ranking_sapal,
+	DENSE_RANK() OVER (ORDER BY widget_output) AS ranking_sapal_2
+FROM	
+	widget_companies wc;
+	
+--- Ranking Within Subgroups with PARTITION BY
+
+CREATE TABLE store_sales (
+	store varchar(30),
+	category varchar(30) NOT NULL,
+	unit_sales bigint NOT NULL,
+	CONSTRAINT store_category_key PRIMARY KEY (store, category)
+);
+
+INSERT INTO store_sales (store, category, unit_sales)
+VALUES
+	('Broders', 'Cereal', 1104),
+	('Wallace', 'Ice Cream', 1863),
+	('Broders', 'Ice Cream', 2517),
+	('Cramers', 'Ice Cream', 2112),
+	('Broders', 'Beer', 641),
+	('Cramers', 'Cereal', 1003),
+	('Cramers', 'Beer', 640),
+	('Wallace', 'Cereal', 980),
+	('Wallace', 'Beer', 988);
+	
+SELECT
+	ss.*,
+	RANK() OVER (PARTITION BY store ORDER BY unit_sales) AS ranking_sapal,
+	ROUND(AVG(unit_sales) OVER (PARTITION BY store),2) AS media
+FROM
+	store_sales ss;
